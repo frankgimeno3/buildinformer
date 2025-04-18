ca53514cf4a81e788ece54da4a5f93cb9a2ea40d
frank gimeno <frankgimeno3@gmail.com>
2025-04-18 03:27:55 +0200

    añado anuncios a rightcol

--- src/app/components/news/RightCol.tsx
+++ src/app/components/news/RightCol.tsx
@@ -6,8 +6,16 @@ interface RightColProps {
 
 const RightCol: FC<RightColProps> = ({ }) => {
   return (
-    <div className='min-h-sceen bg-gray-200 flex-1 hidden md:block'>
-        
+    <div className='min-h-sceen bg-gray-200 flex-1 hidden md:block p-5'>
+        <div className='flex flex-col bg-cyan-900 h-56 py-auto  mb-5'>
+          <p className='text-gray-400 text-center my-auto '>Advert example</p>
+        </div>
+        <div className='flex flex-col bg-cyan-900 h-56 py-auto mb-5 '>
+          <p className='text-gray-400 text-center my-auto '>Advert example</p>
+        </div>
+        <div className='flex flex-col bg-cyan-900 h-56 py-auto mb-5 '>
+          <p className='text-gray-400 text-center my-auto '>Advert example</p>
+        </div>
     </div>
   );
 };
