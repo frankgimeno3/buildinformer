628ed4232a744151fd8e2cf91463794ec5b81fd6
frank gimeno <frankgimeno3@gmail.com>
2025-05-24 15:41:51 +0200

    access

--- src/app/access/page.tsx
+++ src/app/access/page.tsx
@@ -18,7 +18,7 @@ const Access: FC<AccessProps> = ({ }) => {
         <div className="fixed top-0 left-0 w-full z-50">
         <UnloggedNav section={section} menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         </div>
-        <div className="flex flex-col min-h-screen" onClick={()=>{handleCloseMenu()}}>
+        <div className="flex flex-col min-h-screen bg-white text-gray-500" onClick={()=>{handleCloseMenu()}}>
          <p>Access</p>
          <p>Log in</p>
          <p>Sign Up</p>
